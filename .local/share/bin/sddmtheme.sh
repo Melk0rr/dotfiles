a3773ba05fccf4b3e4a02013014669faecbe00ac
Me1k0r <runner-me1k0r@proton.me>
2024-08-10 20:44:52 +0200

    edit: scripts - replaced imagemagick convert instruction

--- .local/share/bin/sddmtheme.sh
+++ .local/share/bin/sddmtheme.sh
@@ -3,6 +3,6 @@
 scrDir=`dirname "$(realpath "$0")"`
 source $scrDir/globalcontrol.sh
 
-convert -density 256x256 -background transparent "${confDir}/sddm/face.svg" -define icon:auto-resize -colors 256 "$HOME/.face.icon"
+magick -density 256x256 -background transparent "${confDir}/sddm/face.svg" -define icon:auto-resize -colors 256 "$HOME/.face.icon"
 
 sudo cp "${confDir}/sddm/themes/corners/theme.conf" "/usr/share/sddm/themes/corners/theme.conf"
