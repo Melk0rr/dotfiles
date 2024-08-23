b3d3be1dc82d2ceeebed70978abf0ba230abc9ed
Me1k0r <runner-me1k0r@proton.me>
2024-08-23 10:26:09 +0200

    edit: rofi - calc rasi config tweaked

--- .local/share/bin/roficalc.sh
+++ .local/share/bin/roficalc.sh
@@ -18,4 +18,4 @@ elem_border=$([ $hypr_border -eq 0 ] && echo "5" || echo $hypr_border)
 r_override="window{border:${hypr_width}px;border-radius:${wind_border}px;} wallbox{border-radius:${elem_border}px;} element{border-radius:${elem_border}px;}"
 r_scale="configuration {font: \"JetBrainsMono Nerd Font ${rofiScale}\";}"
 
-rofi -show calc -theme-str "entry { placeholder: \"Calc...\";}" -theme-str "${r_scale}" -theme-str "${r_override}" -config "${roconf}" -no-sort -no-show-match -calc-command "echo -n '{result}' | cliphist decode"
+rofi -show calc -theme-str "entry { placeholder: \"Calc...\";}" -theme-str "${r_scale}" -theme-str "${r_override}" -config "${roconf}" -no-sort -no-show-match -calc-command "echo -n '{result}' | wl-copy" -automatic-save-to-history
