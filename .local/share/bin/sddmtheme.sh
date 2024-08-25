312542c78fcd717587feaed0a4fbd556b9080665
Me1k0r <runner-me1k0r@proton.me>
2024-08-25 22:47:12 +0200

    edit: sddm - face.icon generation is now handled directly in the face dcol file

--- .local/share/bin/sddmtheme.sh
+++ .local/share/bin/sddmtheme.sh
@@ -3,6 +3,4 @@
 scrDir=`dirname "$(realpath "$0")"`
 source $scrDir/globalcontrol.sh
 
-magick -density 256x256 -background transparent "${confDir}/hypr/face.svg" -define icon:auto-resize -colors 256 "$HOME/.face.icon"
-
 sudo cp "${confDir}/sddm/themes/corners/theme.conf" "/usr/share/sddm/themes/corners/theme.conf"
