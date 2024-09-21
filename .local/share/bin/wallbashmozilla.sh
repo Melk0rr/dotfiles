982fddb72e712f04537cefbc6474000c809d5432
Me1k0r <runner-me1k0r@proton.me>
2024-09-21 21:01:54 +0200

    edit: scripts - hyde update

--- .local/share/bin/wallbashmozilla.sh
+++ .local/share/bin/wallbashmozilla.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env sh
 
-
 #// set variables
-
 scrDir="$(dirname "$(realpath "$0")")"
 source "${scrDir}/globalcontrol.sh"
+
 wallSet="${hydeThemeDir}/wall.set"
 mozProfile=$(cat $HOME/.mozilla/firefox/profiles.ini | grep "Default=" | head -n 1 | awk -F '=' '{print $2}')
 mozDir="$HOME/.mozilla/firefox/${mozProfile}"
