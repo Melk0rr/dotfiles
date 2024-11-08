7462bd1e99e6a3ca3fc77d55dd8ef81d4d4cd27d
Me1k0r <runner-me1k0r@proton.me>
2024-11-08 13:30:13 +0100

    edit: dcols - zen browser in mozilla script

--- .local/share/bin/wallbashmozilla.sh
+++ .local/share/bin/wallbashmozilla.sh
@@ -7,12 +7,16 @@ source "${scrDir}/globalcontrol.sh"
 wallSet="${hydeThemeDir}/wall.set"
 mozProfile=$(grep "Default=" ~/.mozilla/firefox/profiles.ini | head -n 1 | cut -s -f 2 -d =)
 floorpProfile=$(grep "Default=" ~/.floorp/profiles.ini | tail -n 1 | cut -s -f 2 -d '=')
+zenProfile=$(grep "Default=" ~/.zen/profiles.ini | tail -n 1 | cut -s -f 2 -d '=')
 
 mozDir="$HOME/.mozilla/firefox/${mozProfile}"
 floorpDir="$HOME/.floorp/${floorpProfile}"
+zenDir="$HOME/.zen/${zenProfile}"
 
 mkdir -p $mozDir/chrome
 mkdir -p $floorpDir/chrome
+mkdir -p $zenDir/chrome
 
 ln -sf "$(readlink "${wallSet}")" $mozDir/chrome/wall.set
 ln -sf "$(readlink "${wallSet}")" $floorpDir/chrome/wall.set
+ln -sf "$(readlink "${wallSet}")" $zenDir/chrome/wall.set
